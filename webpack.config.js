21f978baa7ee950aca628032b4aa7f20496c9607
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-08-12 13:41:12 -0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      favicon: './src/img/favicon.png',
+    })
+  ],
   module: {
     rules: [
       {
@@ -14,7 +24,11 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ]
   }
 };
